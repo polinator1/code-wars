8ab86c6d7eeb8d11d9fc62fab7c65e789cd2628c
polinator1 <shc-polina@yandex.ru>
2023-10-16 23:18:44 +0300

    corrected wrong answer

--- enumerable_magic_25.js
+++ enumerable_magic_25.js
@@ -3,6 +3,6 @@ Create a function that accepts a list/array and a number n,
 and returns a list/array of the first n elements from the list/array.
 */
 
-function take(arr, n) {
-    return arr.splice(0, n)
+function cookingTime(eggs) {
+    return Math.ceil(eggs / 8) * 5
 }
