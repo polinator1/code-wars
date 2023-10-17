8732126585a3e258d80aee09505e007c62f25e45
polinator1 <shc-polina@yandex.ru>
2023-10-18 01:35:23 +0300

    8 kyu

---
+++ 8KYU/differences_of_volumes_in_cuboids.js
@@ -0,0 +1,24 @@
+/*
+In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which is bigger.
+
+For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. Therefore, the function should return 8.
+
+Your function will be tested with pre-made examples as well as random ones.
+
+If you can, try writing it in one line of code.
+*/
+
+function findDifference(a, b) {
+    let multA = 1
+    let multB = 1
+    
+    a.forEach(nums => {
+      multA *= nums
+    })
+    
+    b.forEach(nums => {
+      multB *= nums
+    })
+    
+    return (multA - multB < 0) ? (multA - multB) * -1 : multA - multB
+}
