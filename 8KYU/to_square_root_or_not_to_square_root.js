684d3b19352f434f24c21af2904303e3b0c8a80e
polinator1 <shc-polina@yandex.ru>
2023-10-18 15:04:43 +0300

    8 kyu

---
+++ 8KYU/to_square_root_or_not_to_square_root.js
@@ -0,0 +1,26 @@
+/*
+Write a method, that will get an integer array as parameter and will process every number from this array.
+
+Return a new array with processing every number of the input-array like this:
+
+If the number has an integer square root, take this, otherwise square the number.
+
+Example
+
+[4,3,9,7,2,1] -> [2,9,3,49,4,1]
+Notes
+
+The input array will always contain only positive numbers, and will never be empty or null.
+*/
+
+function squareOrSquareRoot(array) {
+    let arr = []
+    for (let i = 0; i < array.length; i++) {
+      if (Math.sqrt(array[i]) == Math.round(Math.sqrt(array[i]))) {
+        arr.push(Math.sqrt(array[i]))
+      } else {
+        arr.push(array[i] ** 2)
+      }
+    }
+    return arr;  
+}
